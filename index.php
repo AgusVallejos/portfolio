7bc461bc2295ba5dae76bfbdc7aaae8c3f9dfdfc
AgusVallejos <agustinvallejos010@gmail.com>
2022-07-20 13:36:22 -0300

    Cambios

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+$pg = "inicio"
+?>
+
 <!DOCTYPE html>
 <html lang="es" class="h-100">
 
@@ -15,33 +19,7 @@
 
 <body id="inicio" class="d-flex flex-column h-100">
     <header class="container">
-        <nav class="navbar navbar-expand-md mb-4">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link active px-4 py-1" href="index.html">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="sobre-mi.html">Sobre mí</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="proyectos.html">Proyectos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="contacto.html">Contacto</a>
-                        </li>
-                    </ul>
-                    <div class="d-inline">
-                        <a href="" class="btn btn-rojo">Descargar mi CV <i class="fa fa-download"></i></a>
-                    </div>
-                </div>
-            </div>
-        </nav>
+        <?php include_once "menu.php"; ?>
     </header>
     <main class="container">
         <div class="row">
@@ -54,7 +32,7 @@
             </div>
 
             <div class="col-12 text-center pt-2 mb-5">
-                <a href="proyectos.html" class="btn btn-know  shadow">Conoce mis proyectos</a>
+                <a href="proyectos.php" class="btn btn-know  shadow">Conoce mis proyectos</a>
             </div>
         </div>
     </main>
